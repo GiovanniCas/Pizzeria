872eb1f140a10e7c0ca7b6d00ffc69816f981bb3
GiovanniCas <you@example.comdir>
2022-06-15 16:08:24 +0200

    create viste per ricezione mail

---
+++ resources/js/mio.js
@@ -0,0 +1,18 @@
+// let buttons = document.querySelectorAll('.my-btn');
+// let orders = document.querySelector('.my-tr');
+
+// let counter = 0;
+// buttons.forEach(btn => {
+//     btn.addEventListener('click', function() {
+//         counter++;
+//         if (counter % 2 == 1) {
+            
+//             btn.classList.add('bg-success');
+//         } else {
+//             btn.classList.remove('bg-success');
+
+//         }
+      
+//     });
+    
+// });
---
+++ resources/views/mails/contactMail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Grazie per averci scelto {{$contact["name"]}}!</p>
+    <p>Il tuo ordine è stato preso in carico!</p>
+  
+</body>
+</html>
